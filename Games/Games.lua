d7593d8d431d2d668896d26ecacdfd307b238556
XInfiniteHub <122652224+XInfiniteHub@users.noreply.github.com>
2023-02-20 23:30:42 -0300

    Create Games.lua

---
+++ Games/Games.lua
@@ -0,0 +1,22 @@
+--[[
+    Made by Darkzin
+        { v 1.0.0 }
+    Have fun
+--]]
+
+
+repeat wait() until game:IsLoaded()
+local PlaceId = game.PlaceId
+if PlaceId == 5780309044 or 11423467063 then
+    
+    loadstring(
+    	game:HttpGet("https://raw.githubusercontent.com/XInfiniteHub/Main/main/Central/StandsAwakening/CentralObs.lua",".lua"),
+    true)()
+    
+elseif PlaceId == 4601350214 then
+
+	loadstring(
+    	game:HttpGet("https://raw.githubusercontent.com/XInfiniteHub/Main/main/Shindo%20Life/SLObs.lua",".lua"),
+    true)()
+
+end
