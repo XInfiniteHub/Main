ac99cf601ae7225b480e72d669b7382e6b4b8e48
XInfiniteHub <122652224+XInfiniteHub@users.noreply.github.com>
2023-02-21 00:05:08 -0300

    Update Games.lua

--- Games/Games.lua
+++ Games/Games.lua
@@ -7,12 +7,10 @@
 
 repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
-if PlaceId == 5780309044 or 11423467063 then
-    
+if PlaceId == 5780309044 then
 	loadstring(game:HttpGet"https://raw.githubusercontent.com/XInfiniteHub/Main/main/Central/StandsAwakening/CentralObs.lua")()
-    
+elseif PlaceId == 11423467063 then
+	loadstring(game:HttpGet"https://raw.githubusercontent.com/XInfiniteHub/Main/main/Central/StandsAwakening/CentralObs.lua")()
 elseif PlaceId == 4601350214 then
-
 	loadstring(game:HttpGet"https://raw.githubusercontent.com/XInfiniteHub/Main/main/Shindo%20Life/SLObs.lua")()
-
 end
