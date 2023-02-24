b6156c270ae655639d12b665c8afed2e59602b28
XInfiniteHub <122652224+XInfiniteHub@users.noreply.github.com>
2023-02-24 17:59:56 -0300

    Delete Scripts/Stand Awakening directory

--- Scripts/Stand Awakening/Auto Boss Pc and Mobile/Code.lua
+++
@@ -1,9 +0,0 @@
-local UserInputService = game:GetService("UserInputService")
-
-if UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled and not UserInputService.MouseEnabled then
-  loadstring(game:HttpGet"https://raw.githubusercontent.com/XInfiniteHub/Main/main/MobileVersion/Main/AutoBossMobile.lua",true)()
-	print("Mobile device")
-elseif not UserInputService.TouchEnabled and UserInputService.KeyboardEnabled and UserInputService.MouseEnabled then
-  loadstring(game:HttpGet"https://raw.githubusercontent.com/XInfiniteHub/Main/main/Scripts/Stand%20Awakening/AutoBossPc.lua",true)()
-	print("Computer device")
-end
